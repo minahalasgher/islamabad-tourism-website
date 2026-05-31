b4eb55485d4650d86830a81f375befe677a693e9
Minahil Asghar <minahilasghar218@gmail.com>
2026-05-31 07:35:31 +0500

    Remove duplicate supabase file

--- src/supabase-client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase=createClient(
-    "https://phcayaqdpgftehzgoxuk.supabase.co",
-    "sb_publishable_W81uipOy0u5ecz9-UQisYg_FwZtwqw3",
-);
